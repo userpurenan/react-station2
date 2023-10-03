b56b20e39052e190cbdc53857f8a546a4d10f71a
userpurenan <nan691628@gmail.com>
2023-10-04 00:35:39 +0900

    APIのベースurlを「.env」にまとめた

--- src/Thread/ThreadList.js
+++ src/Thread/ThreadList.js
@@ -3,12 +3,13 @@ import { ThreadListHeader } from './ThreadListHeader';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from 'react-router-dom';
+import { url } from '../const';
 
 export const ThreadList = () => {
   const [threads, SetThreads] = useState([]);
 
   useEffect(() => {
-    axios.get("https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads")
+    axios.get(`${url}/threads`)
       .then((response) => {
         //console.log(response);
         SetThreads(response.data); //スレッドにAPIから取ってきた値をセット
